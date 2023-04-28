80cee316a3334476233d7549e37aa4ce9eb4b7e4
Grabe-me <grabe85me@gmail.com>
2023-04-28 13:53:28 +0300

    Docker pushed

--- menu_app/settings.py
+++ menu_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^wa5!niz=cp4)21j4f*^9e(oyk%j4&ov&a9)6y(x1!t*29@xyz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
 
 # Application definition
